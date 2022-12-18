2a1e2e504539ec0b0e182a1fa4a11b1af8d928ee
oleg <olazeba@fondy.eu>
2022-12-18 18:09:05 +0200

    first

---
+++ sweater/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.secret_key = 'zapel'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+
+from sweater import models, routes
+
+
+db.create_all()
